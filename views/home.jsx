2267d9466d7fa0e43622d2d1de61fb7e88ff8160
James <jfnelson2000@gmail.com>
2022-11-28 19:14:22 -0800

    adding styles to RESTRant

--- views/home.jsx
+++ views/home.jsx
@@ -4,8 +4,14 @@ const Def = require('./default')
 function home () {
     return (
       <Def>
+        <head>
+          <link rel="stylesheet" href="./style.css" />
+        </head>
           <main>
               <h1>HOME</h1>
+              <a href="/places">
+              <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </Def>
     )
