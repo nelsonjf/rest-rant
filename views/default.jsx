e3665914911fabd4a965d152430d63f9cdcb78b4
James <jfnelson2000@gmail.com>
2022-12-10 10:18:29 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,19 @@ function Def (html) {
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Places</a>
+                        </li>
+                        <li>
+                            <a href="">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
